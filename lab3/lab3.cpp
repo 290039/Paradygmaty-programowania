e67855c3d2d6ce8b9c73194dd801a3168e0c4574
Jaedite <290039@student.pwr.edu.pl>
2026-04-25 11:19:34 +0200

    Zmodyfikowano kod o klasę interfejs użytkownika

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
-#define N 10 // max osob
-#define M 10 // max list
+#define ilosc_osob 10 // max osob
+#define ilosc_list 2 // max list
 using namespace std;
 
 class Osoba
@@ -54,74 +54,33 @@ class Osoba
         }
 };
 
-Osoba* studenci[N] = {};
-
-void dodajStudenta(string nazwisko, string imie, int indeks) {
-    for (int i = 0; i < N; i++)
-    {
-        if (studenci[i] == nullptr)
-        {
-            studenci[i] = new Osoba(nazwisko, imie, indeks);
-            return;
-        }
-    }
-    cout << "Lista pełna\n";
-}
+Osoba* studenci[ilosc_osob] = {};
 
 
-Osoba* wyszukajPoIndeksie(Osoba* studenci[], int indeks) {
-    for (int i = 0; i < N; i++)
-    {
-        if (studenci[i] != nullptr && studenci[i] -> getIndeks() == indeks)
-        {
-            return studenci[i];
-        }
-    }
-    return nullptr;    
-}
-
-void wyswietlListeStudentow(Osoba* studenci[]) 
-{
-   int count = 0;
-   for (int i = 0; i < N; i++)
-   {
-    if (studenci[i] != nullptr)
-    {
-        studenci[i]->wyswietl();
-        count++;
-    }
-    
-   }
-   if (count == 0)
-   {
-    cout << "Lista studentów jest pusta.\n";
-   }
-   
-}
 
 class ListaObecnosci
 {
     private:
-        Osoba* osoby[N];
-        bool obecnosc[N];
-        int rozmiar;
+        Osoba* tablicaOsob[ilosc_osob];
+        bool tablicaObecnosci[ilosc_osob];
+        int rozmiar = 0;
         
 
     public:
         ListaObecnosci()
         {
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < ilosc_osob; i++)
             {
-                osoby[i] = nullptr;
-                obecnosc[i] = false;
+                tablicaOsob[i] = nullptr;
+                tablicaObecnosci[i] = false;
             }
         }
 
-void dodajOsobę(Osoba* o)
+void dodajOsobe(Osoba* o)
     {
-        if (rozmiar < N)
+        if (rozmiar < ilosc_osob)
         {
-            osoby[rozmiar] = o;
+            tablicaOsob[rozmiar] = o;
             rozmiar++;
         }
 
@@ -132,25 +91,42 @@ void dodajOsobę(Osoba* o)
         
     }
 
-    void ustawObecnosc(int indeks, bool sprawdzenie) {
+
+    void ustawObecnosc(int indeks, bool obecnosc) {
     for (int i = 0; i < rozmiar; i++)
     {
-        if (osoby[i] != nullptr && osoby[i]->getIndeks() == indeks)
+        if (tablicaOsob[i] != nullptr && tablicaOsob[i]->getIndeks() == indeks)
             {
-                obecnosc[i] = sprawdzenie;
+                tablicaObecnosci[i] = obecnosc;
             }
     }   
 }
+
+void usunzlisty(int indeks) 
+{
+    for (int i = 0; i < ilosc_osob; i++)
+    {
+        if (tablicaOsob[i] != nullptr && tablicaOsob[i]->getIndeks() == indeks)
+        {
+            tablicaOsob[i] = nullptr;
+            tablicaObecnosci[i] = false;
+            cout << "Osoba została usunieta z listy\n";
+        }
+        
+    }
+    
+}
+
 
 void wyswietl()
 {
     for (int i = 0; i < rozmiar; i++)
     {
-        if (osoby[i] != nullptr)
+        if (tablicaOsob[i] != nullptr)
         {
-            cout << osoby[i]->getNazwisko() << " " << osoby[i]->getImie() << " " << osoby[i]->getIndeks() << " ";
+            cout << tablicaOsob[i]->getNazwisko() << " " << tablicaOsob[i]->getImie() << " " << tablicaOsob[i]->getIndeks() << " ";
 
-            if (obecnosc[i])
+            if (tablicaObecnosci[i])
             {
                 cout << "Obecny";
             }
@@ -166,49 +142,222 @@ void wyswietl()
 
 };
 
-ListaObecnosci* listy[M] = {};
-int liczbaList = 0;
+class InterfejsUzytkownika 
+{
 
-int main() {
+    private:
+    Osoba **tablicaOsob;
+    int iloscOsob;
+    ListaObecnosci **tablicaList;
+    int iloscList;
 
-string nazwisko, imie;
-int indeks, input;
+void wyswietlListeStudentow(Osoba* studenci[]) 
+{
+   int count = 0;
+   for (int i = 0; i < ilosc_osob; i++)
+   {
+    if (studenci[i] != nullptr)
+    {
+        studenci[i]->wyswietl();
+        count++;
+    }
+    
+   }
+   if (count == 0)
+   {
+    cout << "Lista studentów jest pusta.\n";
+   }
+   
+}
+
+Osoba* wyszukajPoIndeksie(int indeks) {
+    for (int i = 0; i < ilosc_osob; i++)
+    {
+        if (studenci[i] != nullptr && studenci[i] -> getIndeks() == indeks)
+        {
+            return studenci[i];
+        }
+    }
+    return nullptr;    
+}    
+
+    public:
+    InterfejsUzytkownika(Osoba **tab_osob, int il_osob, ListaObecnosci **tab_list, int il_list)
+{
+    tablicaOsob = tab_osob;
+    iloscOsob = il_osob;
+
+    tablicaList = tab_list;
+    iloscList = il_list;
+}
+
+void dodajStudenta(string nazwisko, string imie, int indeks) {
+    for (int i = 0; i < ilosc_osob; i++)
+    {
+        if (studenci[i] == nullptr)
+        {
+            studenci[i] = new Osoba(nazwisko, imie, indeks);
+            return;
+        }
+    }
+    cout << "Lista pełna\n";
+}
+
+void dodajListe() {
+    for (int i = 0; i < ilosc_list; i++)
+    {
+        if (tablicaList[i] == nullptr)
+        {
+            tablicaList[i] = new ListaObecnosci();
+            iloscList++;
+            cout << "Dodano liste o numerze: " << i << " do bazy list\n";
+            return;
+        }
+    }
+    cout << "Lista pelna\n";
+}
+
+    void loop() 
+    {
+        
+    string t_nazwisko, t_imie;
+    int t_indeks, input, wybrana_lista;
 
     while(true) {
 
     cout << "\n";
-    cout << "1: Dodaj Osobe\n";
-    cout << "2: Wyświetl\n";
-    cout << "3: Zmien dane\n";
-    cout << "4: Stworz liste\n";
-    cout << "5: Dodaj osobe do listy\n";
-    cout << "6: Sprawdz obecnosc\n";
-    cout << "7: Wyswietl\n";
-    cout << "8: Koniec\n";
+    cout << "1: Dodaj Osobe do bazy osob\n";
+    cout << "2: Dodaj liste obecnosci do bazy list\n";
+    cout << "3: Wybierz liste obecnosci z ktora chcesz pracowac\n";
+    cout << "4: Wyswietl baze osob\n";
+    cout << "5: Edytuj dane osob w bazie\n";
+    cout << "6: Zakoncz\n";
+    cout << "Wybrana opcja: \n";
+
+    cin >> input;
+    
     cout << "\n";
 
     cin >> input;
     switch (input)
     {
-    case 1:
+    case 1: //Dodaj Osobe
 
     cout << "Podaj nazwisko: \n";
-    cin >> nazwisko;
+    cin >> t_nazwisko;
     cout << "Podaj imie: \n";
-    cin >> imie;
+    cin >> t_imie;
     cout << "Podaj indeks: \n";
-    cin >> indeks;
+    cin >> t_indeks;
 
-    dodajStudenta(nazwisko, imie, indeks);
+    dodajStudenta(t_nazwisko, t_imie, t_indeks);
 
     break;
 
-    case 2:
-        wyswietlListeStudentow(studenci);
-        break;
+case 2: //Dodaj liste obecnosci
 
-    case 3:
+        dodajListe();
+
+        break;
+    
+case 3: //Wybierz liste obecnosci z ktora chcesz pracowac
+
+        {
+    if (iloscList == 0)
     {
+        cout << "Brak list w bazie.\n";
+        break;
+    }
+
+    int numerListy;
+    cout << "Podaj numer listy (Od 1 do " << iloscList << "): ";
+    cin >> numerListy;
+
+    if (numerListy >= 1 && numerListy <= iloscList)
+    {
+        bool wyjdzZListy = false;
+
+        while (!wyjdzZListy)
+        {
+            cout << "Pracujesz z listą o numerze: " << numerListy << endl;
+            cout << "1. Wyswietl liste" << numerListy << endl;
+            cout << "2. Dodaj osobe do listy " << numerListy << endl;
+            cout << "3. Zmien obecnosc osoby na liscie " << numerListy << endl;
+            cout << "4. Usun osobe z listy " << numerListy << endl;
+            cout << "5. Powrot do menu glownego" << endl;
+            cout << "Wybrana opcja: \n";
+            cin >> wybrana_lista;
+            cout << "\n";
+
+            switch (wybrana_lista) //Wyswietl
+            {
+            case 1:
+                tablicaList[numerListy - 1]->wyswietl();
+                break;
+
+            case 2: //Dodaj
+            {
+                cout << "Podaj indeks osoby do dodania: ";
+                cin >> t_indeks;
+                Osoba* o = wyszukajPoIndeksie(t_indeks);
+                if (o != nullptr)
+                    tablicaList[numerListy - 1]->dodajOsobe(o);
+                break;
+            }
+
+            case 3: //Zmien
+            {
+                int t_indeks, zmiana;
+                cout << "Podaj indeks osoby: ";
+                cin >> t_indeks;
+                cout << "1. Obecny\n";
+                cout << "2. Nieobecny\n";
+                cout << "Wybrana opcja: ";
+                cin >> zmiana;
+
+                if (zmiana == 1)
+                    tablicaList[numerListy - 1]->ustawObecnosc(t_indeks, true);
+                else if (zmiana == 2)
+                    tablicaList[numerListy - 1]->ustawObecnosc(t_indeks, false);
+                else
+                    cout << "Nieprawidlowa opcja.\n";
+                break;
+            }
+
+            case 4: //Usun
+            {
+                int t_indeks;
+                cout << "Podaj indeks osoby do usuniecia: ";
+                cin >> t_indeks;
+                tablicaList[numerListy - 1]->usunzlisty(t_indeks);
+                break;
+            }
+
+            case 5:
+                wyjdzZListy = true;
+                break;
+
+            default:
+                cout << "Brak takiej opcji.\n";
+                break;
+            }
+        }
+    }
+    else
+    {
+        cout << "Nieprawidlowy numer listy.\n";
+    }
+    break;
+}
+
+case 4: //Wyswietl baze osob
+        
+        wyswietlListeStudentow(tablicaOsob);
+        break;
+        
+case 5: //Edytuj dane osoby w bazie
+
+{
     int zmiana, nowyIndeks;
     
     cout << "\n";
@@ -221,8 +370,8 @@ int indeks, input;
     cin >> zmiana;
 
         cout << "Podaj indeks osoby, ktorej dane chcesz zmienic: \n";
-        cin >> indeks;
-        Osoba* osoba = wyszukajPoIndeksie(studenci, indeks);
+        cin >> t_indeks;
+        Osoba* osoba = wyszukajPoIndeksie(t_indeks);
 
         if (osoba == nullptr)
         {
@@ -241,13 +390,13 @@ int indeks, input;
             break;
         case 2:
             cout << "Podaj nowe nazwisko: \n";
-            cin >> nazwisko;
-            osoba->setNazwisko(nazwisko);
+            cin >> t_nazwisko;
+            osoba->setNazwisko(t_nazwisko);
             break;
         case 3:
             cout << "Podaj nowe imie: \n";
-            cin >> imie;
-            osoba->setImie(imie);
+            cin >> t_imie;
+            osoba->setImie(t_imie);
             break;
 
         case 4:
@@ -261,71 +410,28 @@ int indeks, input;
         break;
     }
 
-    case 4: //Stworz liste
-        if (liczbaList < M)
-        {
-            listy[liczbaList++] = new ListaObecnosci();
-            cout << "Utworzono liste: " << liczbaList << endl;
-        }
-        break;
-        
-    case 5: //Dodaj osobe do listy
-        {
-        int numerListy;
-        cout << "Podaj numer wybranej listy od 1 do " << liczbaList << ": ";
-        cin >> numerListy;
-
-        cout << "Podaj indeks osoby do dodania: ";
-        cin >> indeks;
-        Osoba* o = wyszukajPoIndeksie(studenci, indeks);
-        
-        listy[numerListy - 1]->dodajOsobę(o);
-
-        cout << "Dodano osobę do listy.\n";
-        }
-         break;
-
-    case 6: //Sprawdz obecnosc
-{
-    int numerListy;
-    bool obecny;
-
-    cout << "Podaj numer listy: ";
-    cin >> numerListy;
-
-    cout << "Podaj indeks: ";
-    cin >> indeks;
-
-    cout << "Obecnosc 0/1 ?";
-    cin >> obecny;
-
-    if (numerListy <= liczbaList)
-    {
-        listy[numerListy - 1]->ustawObecnosc(indeks, obecny);
-    }
-
-    break;
-}
-    
-    case 7: //Wyswietl
-{
-    int numerListy;
-    cout << "Podaj numer listy: ";
-    cin >> numerListy;
-
-    if (numerListy <= liczbaList)
-    {
-        listy[numerListy - 1]->wyswietl();
-    }
-
-    break;
-}
-
-    case 8:
-      return 0;
+case 6: //Zakoncz
+      return;
     }
     
 }
+}
+
+};
+
+
+
+ListaObecnosci* listy[ilosc_list] = {};
+int liczbaList = 0;
+
+int main() {
+
+    Osoba* tablicaOsob[ilosc_osob] = {};          
+    ListaObecnosci* tablicaList[ilosc_list] = {};  
+
+    InterfejsUzytkownika ui(tablicaOsob, ilosc_osob, tablicaList, ilosc_list);  
+    ui.loop();
+
     return 0;
 }
 
